2ba7b19017108199c5a48800085fc6618ed091c3
Ripal Bhavsar <ripal_bhavsar@yahoo.com>
2023-10-13 12:40:41 +0200

    test

--- main.tf
+++ main.tf
@@ -1,25 +1,30 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
+# resource "aws_instance" "ec2_instance" {
+#   ami=var.AMIS[var.AWS_REGION] 
+#   instance_type = "t3.micro"
+#   associate_public_ip_address = true
+#   tags = {
+#     Name = "Terraform_test_ec2"
+#   }
+# }
+# output public_ip{
+#   value=aws_instance.ec2_instance.public_ip
+# }
+# resource "aws_instance" "webserver" {
+#   for_each = var.webservers
+#     ami=var.AMIS[var.AWS_REGION] 
+#     instance_type = "t2.micro"
+#     associate_public_ip_address = true
+#     tags = {
+#       Name = each.value["name"]
+#     }
+# }
+data "aws_availability_zones" "available"{}
+# VPC Creation using CIDR block available in vars.tf
+resource "aws_vpc" "provisionerVPC"{
+    cidr_block = var.vpc_cidr
+    enable_dns_hostnames=true
+    enable_dns_support = true
+    tags = {
+        Name = "rgb-vpc"
     }
-  }
-
-  required_version = ">= 0.14.9"
-}
-
-#Provider profile and region in which all the resources will create
-provider "aws" {
-  profile = "default"
-  region  = "us-west-1"
-}
-
-#Resource to create s3 bucket
-resource "aws_s3_bucket" "rbsbucket"{
-  bucket = "ck-demo-bucket"
-
-  tags = {
-    Name = "rbsbucket"
-  }
 }
